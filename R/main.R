28daeb1c6e4dbb34cf24bdb63751ce3224e78a15
Sunny Tseng <sunnyyctseng@gmail.com>
2024-04-07 20:55:12 -0700

    added map information

--- R/main.R
+++ R/main.R
@@ -14,3 +14,32 @@ my_data <- bbs_GBIF_subset(folder = here::here("data", "dwca-bbstaiwan_dataset-v
 
 
 library(terra)
+library(here)
+library(tidyverse)
+library(tidyterra)
+
+## map of taiwan, from https://geodata.mit.edu/catalog/stanford-dz142zj5454
+## CRS: lon/lat WGS 84 (EPSG:4326)
+twmap <- terra::vect(here::here("data", "taiwan"))
+
+
+## map of important bird region, from https://age.triwra.org.tw/Page/MapLayerDataList
+## CRS: lon/lat WGS 84 (EPSG:4326)
+birdmap <- terra::vect(here::here("data", "重要野鳥棲地"))
+
+
+## create SpatExtent
+aoi <- birdmap %>%
+  ext() %>%
+  + c(0 , 0.1, 0, -1)
+
+
+## create map
+distribution_map <- ggplot() +
+  geom_spatvector(data = twmap %>% crop(aoi), fill = "white") +
+  geom_spatvector(data = birdmap %>% crop(aoi), fill = "green", alpha = 0.2, colour = NA) +
+  theme_bw()
+distribution_map
+
+
+
