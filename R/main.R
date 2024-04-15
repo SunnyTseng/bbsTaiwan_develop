1b1a99aedfb2fd6b0e3e513fa4bfec3d233a0583
Sunny Tseng <sunnyyctseng@gmail.com>
2024-04-14 22:03:51 -0700

    optimized the three functions

--- R/main.R
+++ R/main.R
@@ -6,16 +6,17 @@ source(here::here("R", "bbs_translation.R"))
 source(here::here("R", "bbs_subset.R"))
 source(here::here("R", "bbs_visualization.R"))
 
+source(here::here("R", "required_files_preparation.R"))
 
 # example code for demonstration ------------------------------------------
 
-bbs_translation(species = "白頭翁")
-bbs_translation(species = "烏頭翁")
+bbs_translation(species = "山紅頭")
+bbs_translation(species = "巨嘴鴉")
 
 occurrence_sub <- bbs_GBIF_subset(folder = here::here("data", "dwca-bbstaiwan_dataset-v1.9"),
                                   from = 2010,
                                   to = 2012,
-                                  species_list = c("Pycnonotus sinensis", "Pycnonotus taivanus"))
+                                  species_list = c("Cyanoderma ruficeps", "Corvus macrorhynchos"))
 
 bird_plot <- bbs_visualization(data = occurrence_sub)
 bird_plot
